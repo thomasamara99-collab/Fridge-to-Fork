faa4f4e9fa0ebdef20df2690f4ef9668c4bf19dd
Thomas Amara <thomas.amara@email.com>
2026-03-24 17:06:20 +0100

    auth/api fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { z } from "zod";
 
 import { prisma } from "./prisma";
